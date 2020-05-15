17188d0df050d6a174a83f532bd5c643babb38db
shengluo <914001985@qq.com>
2020-05-15 15:28:17 +0800

    无重复字符的最长子串

---
+++ demo-eureka-service-user/src/test/java/com/example/demo/LengthOfLongestSubstring.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int longest = s.length();
+        String[] strArray = s.split("");
+        for(;longest >0 ; longest--) {
+
+        }
+        return longest;
+    }
+}
