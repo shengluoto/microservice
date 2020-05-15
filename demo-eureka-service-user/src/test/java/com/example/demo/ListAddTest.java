84df0be987e0cc6e43a324028b62845e5b7313f3
shengluo <914001985@qq.com>
2020-05-15 13:40:15 +0800

    链表相加

--- demo-eureka-service-user/src/test/java/com/example/demo/ListAddTest.java
+++ demo-eureka-service-user/src/test/java/com/example/demo/ListAddTest.java
@@ -12,46 +12,33 @@ class ListAddTest {
 	}
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode result = null;
-		int times = 0;
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
 		int l1Value = 0;
 		int l2Value = 0;
-		int tempValue = 0;
+		int sum = 0;
 		int remainderValue = 0;
 		int carryValue = 0;
-		while(null != l1 || null != l2 || carryValue !=0) {
+		while(null != p || null != q) {
 			// 值相加，并带上进位
-			l1Value = null==l1 ? 0 : l1.val;
-			l2Value = null==l2 ? 0 : l2.val;
-			tempValue = l1Value + l2Value + carryValue;
+			l1Value = null==p ? 0 : p.val;
+			l2Value = null==q ? 0 : q.val;
+			sum = l1Value + l2Value + carryValue;
 			// 当前计算值的个位
-			remainderValue = tempValue%10;
+			remainderValue = sum%10;
 			// 进位
-			carryValue = tempValue >= 10 ? 1 : 0;
+			carryValue = sum >= 10 ? 1 : 0;
 
-			if (times==0) {
-				result = new ListNode(remainderValue);
-                last = result;
-            } else {
-                this.add(remainderValue);
-			}
-			if (null == l1 && null == l2) {
-				break;
-			}
-            times++;
-			l1 = l1.next;
-			l2 = l2.next;
+			curr.next = new ListNode(remainderValue);
+			curr = curr.next;
+			if (p != null) p = p.next;
+			if (q != null) q = q.next;
 		}
-		return result;
+		if (carryValue > 0) {
+			curr.next = new ListNode(carryValue);
+		}
+		return dummyHead.next;
 	}
-
-	private ListNode last;
-
-	private void add(int value) {
-	    ListNode current = last;
-	    ListNode newNode = new ListNode(value);
-        current.next = newNode;
-    }
 
 	@Test
 	public void test() {
@@ -64,5 +51,25 @@ class ListAddTest {
 		l2.next.next = new ListNode(4);
 
 		this.addTwoNumbers(l1, l2);
+	}
+
+	public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		while (p != null || q != null) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null) p = p.next;
+			if (q != null) q = q.next;
+		}
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
 	}
 }
