6ffb848d7a6d9acf3842edef7ebd1b4ec5b4bad3
shengluoto <914001985@qq.com>
2020-05-21 22:57:13 +0800

    算法每日练习

--- demo-eureka-service-user/src/test/java/com/example/demo/LengthOfLongestSubstring.java
+++ demo-eureka-service-user/src/test/java/com/example/demo/LengthOfLongestSubstring.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LengthOfLongestSubstring {
@@ -27,6 +28,35 @@ public class LengthOfLongestSubstring {
                 return longest;
             }
             start++;
+        }
+        return longest;
+    }
+
+    public int lengthOfLongestSubstring4(String s) {
+        int longest = 0;
+        int length = s.length();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i = 0; i < length; i ++){
+
+            map.put(s.charAt(i), i);
+            longest =0;
+        }
+        return longest;
+    }
+
+    public int lengthOfLongestSubstring5(String s) {
+        int longest = 0;
+        int length = s.length();
+        int left = 0;
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < length; i++){
+            if (set.add(s.charAt(i))) {
+                continue;
+            }
+            set.remove(s.charAt(i));
+            longest = Math.max(longest, i-left);
+            --i;
+            ++left;
         }
         return longest;
     }
@@ -64,5 +94,59 @@ public class LengthOfLongestSubstring {
         }
 
         return res;
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        int result = 0;
+        int remain = 0;
+        while (temp!=0) {
+            remain = temp%10;
+            temp = temp/10;
+            result = result*10 + remain;
+        }
+        return x == result ? true : false;
+    }
+
+    public static int romanToInt(String s) {
+        Map<Integer, Integer> combinationMap = new HashMap<>();
+        combinationMap.put('I'+'V', 4);
+        combinationMap.put('I'+'X', 9);
+        combinationMap.put('X'+'L', 40);
+        combinationMap.put('X'+'C', 90);
+        combinationMap.put('C'+'D', 400);
+        combinationMap.put('C'+'M', 900);
+        Map<Character, Integer> singleMap = new HashMap<>();
+        singleMap.put('I', 1);
+        singleMap.put('V', 5);
+        singleMap.put('X', 10);
+        singleMap.put('L', 50);
+        singleMap.put('C', 100);
+        singleMap.put('D', 500);
+        singleMap.put('M', 1000);
+        int length = s.length();
+        int start = 0;
+        int num = 0;
+        int result = 0;
+        Integer tempStr = null;
+        while (start < length) {
+            tempStr = s.charAt(start)+s.charAt(start+1);
+            if (combinationMap.containsKey(tempStr)) {
+                num = combinationMap.get(tempStr);
+                start +=2;
+            } else {
+                num = singleMap.get(s.charAt(start));
+                ++start;
+            }
+            result += num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LengthOfLongestSubstring.romanToInt("III");
     }
 }
